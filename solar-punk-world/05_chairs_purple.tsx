be03dd0a0550b07b63a81f4ba0b012456e370074
transmeaning <64962292+transmeaning@users.noreply.github.com>
2020-12-24 22:10:53 +0100

    adding main asset folder

---
+++ solar-punk-world/05_chairs_purple.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.12.18" name="05_chairs_purple" tilewidth="32" tileheight="32" tilecount="4" columns="4">
+ <image source="../../../Downloads/starter-kit-2d-world-rc3-master/starter-kit-2d-world-rc3-master/tiles_png/community/cert/imgs/furniture/05_chairs_purple.png" width="128" height="32"/>
+</tileset>
